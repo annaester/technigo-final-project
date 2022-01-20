43d1ef2b2056654b9369e7a3b33a4f954eecac62
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2022-01-20 09:27:11 +0100

    reseeded db

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://trivia-elsalisa-api.herokuapp.com";
+
+//"http://localhost:8080";
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
