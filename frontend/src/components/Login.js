7d7742ce75ece7000878da21afc4f9a5e450cbf0
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2022-02-09 17:56:55 +0100

    fixed warning

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../utils/constants";
 import member from "../reducers/member";
 import Rules from "./Rules";
-import styled from "styled-components";
 import {
   Background,
   LoginBox,
